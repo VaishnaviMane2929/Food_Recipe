4e6dba901b54c64b384568a6acace8e9885b4b55
vaishnavi <vaishnavimane991@gmail.com>
2025-01-10 14:22:40 +0530

    search bar error

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Mainpage from './Components/Mainpage';
+import {Route,Routes} from 'react-router-dom';
 
 function App() {
   return (
-   <>
-   <Mainpage/>
-   </>
+    <>
+    <Mainpage/>
+    <Routes>
+      <Route path='/' element={<Mainpage/>}/>
+
+    </Routes>
+    </>
+    
+
+    
+
   );
 }
 
--- src/Components/Mealcards.js
+++ src/Components/Mealcards.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
 const Mealcards = ({detail}) => {
     console.log(detail);
     
   return (
-    <div>Mealcards</div>
+    <div className='meals'>
+      {!detail ? "" : detail.map((curItem) => {
+        return (
+               <div className='mealImg'>
+               <img src={curItem.strMealThumb}/>
+               <p>{curItem.strMeal}</p>
+               <NavLink><button>Recipe</button>
+               </NavLink>
+               
+               </div>
+        )
+      })
+     
+      }
+      
+    </div>
   )
 }
 
